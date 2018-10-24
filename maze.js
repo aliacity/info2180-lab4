2d59b600cd0d38e2e500637e65b9f5d97a213dd9
aliacity <aliacity@users.noreply.github.com>
2018-10-23 20:10:32 -0500

    commit after exercise 2

--- maze.js
+++ maze.js
@@ -6,3 +6,29 @@ document.getElementById("boundary1").addEventListener("mouseover", function lose
 		document.getElementById("boundary1").classList.add("youlose");
 	}
 )
+
+
+for ( var x = 0; x < document.getElementsByClassName("boundary").length - 1; x++)
+		{
+
+			document.getElementsByClassName("boundary")[x].addEventListener("mouseover", function loser2()
+					{
+						for (var y = 0; y < document.getElementsByClassName("boundary").length - 1; y++)
+						{
+
+						document.querySelectorAll("div.boundary")[y].classList.add("youlose");
+
+						}
+
+							let losetext = document.createElement("h2");
+							losetext.textContent = "You Lose!";
+						  document.body.append(losetext);
+						  var status = document.createAttribute("id");
+							status.value = "status";
+							losetext.setAttributeNode(status);
+
+
+					})
+		}
+
+//Excercise 3
